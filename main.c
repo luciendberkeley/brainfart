d706961a7f55868008b7f0672852f11f75bd44d6
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-02-03 17:06:25 -0800

    Memory created and simple commands done, need to read input and do loops

--- main.c
+++ main.c
@@ -13,9 +13,9 @@ int getInstructionChars(char outPoint[], char filePath[]) {
   FILE *filePtr;
 
   // Windows:
-  // fopen_s(&filePtr, filePath, "r");
+  fopen_s(&filePtr, filePath, "r");
   // Other:
-  filePtr = fopen(filePath, "r");
+  // filePtr = fopen(filePath, "r");
 
   int instructionIndex = 0;
 
@@ -42,27 +42,89 @@ int getInstructionChars(char outPoint[], char filePath[]) {
   return instructionIndex;
 }
 
+void processInstructions(char instructions[], int count, uint8_t memory[], int *pointer, int size) {
+  printf("\n\nPROGRAM:\n");
+  printf("---------------------------------\n\n");
+
+  *pointer = 0;
+  for(int i=0; i < size; i++) {
+    memory[i] = 0;
+  }
+  
+
+  for(int i=0; i < count; i++) {
+    switch(instructions[i]) {
+      case 62:
+        // Right
+        (*pointer)++;
+        break;
+      case 60:
+        // Left
+        (*pointer)--;
+        break;
+      case 43:
+        // Incr
+        memory[*pointer]++;
+        break;
+      case 45:
+        // Decr
+        memory[*pointer]--;
+        break;
+      case 46:
+        // Output
+        printf("%c", memory[*pointer]);
+        break;
+      case 44:
+        // Input byte
+        break;
+      case 91:
+        // Loop start
+        break;
+      case 93:
+        // Loop end
+        break;
+    }
+  }
+  
+  printf("\n\n---------------------------------\n");
+  printf("END PROGRAM\n\n");
+}
+
+void printMemory(uint8_t memory[], int *pointer, int size) {
+  printf("Memory:\n[");
+  for(int i=0; i < size; i++) {
+    printf("%s%i%s", i == *pointer ? "P|> " : "" ,memory[i], i != size - 1 ? ", " : "");
+  }
+  printf("]. Pointer Pos: %i\n", *pointer);
+}
+
 int main() {
   printf("Starting\n\n");
 
   printf("Parse Tokens\n");
   char instructionChars[128];
-  int size = getInstructionChars(instructionChars, "test.txt");
+  int instructionCount = getInstructionChars(instructionChars, "test.txt");
 
-  printf("Readable:\n");
-  for(int i=0; i < size; i++) {
-    printf("\"%c\", ", instructionChars[i]);
+  printf("Readable:\n[");
+  for(int i=0; i < instructionCount; i++) {
+    printf("\"%c\"%s", instructionChars[i], i != instructionCount - 1 ? ", " : "");
   }
+  printf("]\n\n");
 
-  printf("\n\nChars:\n");
+  printf("Chars:\n[");
   
-  for(int i=0; i < size; i++) {
-    printf("\"%u\", ", instructionChars[i]);
+  for(int i=0; i < instructionCount; i++) {
+    printf("%u%s", instructionChars[i], i != instructionCount - 1 ? ", " : "");
   }
-  printf("\n");
+  printf("]\n\n");
 
   // All the execution code and memory will go here later on
-	
+  int memorySize = 10;
+	uint8_t memory[memorySize];
+  int pointer = 0;
+  processInstructions(instructionChars, instructionCount, memory, &pointer, memorySize);
+
+  printMemory(memory, &pointer, memorySize);
 
   return 0;
 }
